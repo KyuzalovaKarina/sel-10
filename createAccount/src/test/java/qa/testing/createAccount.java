7b9f709e7ec8c57b2baf1e5d358752f0d40248e6
KARINA\Карина <karina_crystal@mail.ru>
2017-09-07 01:05:18 +0500

    fix task11

--- createAccount/src/test/java/qa/testing/createAccount.java
+++ createAccount/src/test/java/qa/testing/createAccount.java
@@ -10,9 +10,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.Random;
 
-public class createAccount {
+public class CreateAccount {
     private WebDriver driver;
     private WebDriverWait wait;
 
